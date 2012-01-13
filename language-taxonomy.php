528648e10cf3e4248bd17ac174bee07d6e5831e1
Dominik Schwind <dschwind@lostfocus.de>
2012-01-13 18:01:59 +0100

    First commit of language taxonomy plugin / ref 255176

---
+++ language-taxonomy.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Plugin Name: Language Taxonomy
+Plugin URI: http://www.lostfocus.de/language-taxonomy/
+Description: Adds a language taxonomy to posts, pages and other items.
+Version: 0.1
+Author: Dominik Schwind
+Author URI: http://www.lostfocus.de/
+*/
+function create_language_taxonomies() 
+{
+	$labels = array(
+		'name' => _x( 'Language', 'taxonomy general name' ),
+		'singular_name' => _x( 'Language', 'taxonomy singular name' ),
+		'search_items' =>  __( 'Search Languages' ),
+		'popular_items' => __( 'Popular Languagess' ),
+		'all_items' => __( 'All Languages' ),
+		'parent_item' => null,
+		'parent_item_colon' => null,
+		'edit_item' => __( 'Edit Language' ), 
+		'update_item' => __( 'Update Language' ),
+		'add_new_item' => __( 'Add New Language' ),
+		'new_item_name' => __( 'New Language' ),
+		'separate_items_with_commas' => __( 'Separate languages with commas' ),
+		'add_or_remove_items' => __( 'Add or remove languages' ),
+		'choose_from_most_used' => __( 'Choose from the most used languages' )
+		); 
+
+	register_taxonomy('language', array('post','page'), array(
+		'hierarchical' => false,
+		'labels' => $labels,
+		'show_ui' => true,
+		'query_var' => true,
+		'rewrite' => array( 'slug' => 'lang' ),
+		));
+
+}
+
+add_action( 'init', 'create_language_taxonomies', 0 );
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Language Taxonomy ===
+Contributors: dominik.schwind
+Tags: language, taxonomy
+Requires at least: 3.0
+Tested up to: 3.0
+Stable tag: 0.1
+
+Adds a language taxonomy to posts, pages and other items.
+
+== Description ==
+
+Adds a language taxonomy to posts, pages and other items.
+
+This will add a URL scheme (Your Blog)/lang/(slug) - that's basically it, at the moment.
+
+== Installation ==
+
+1. Just like any other plugin, too.
+1. Activate it.
+1. Update your "Permalinks" settings.
+
+== Changelog ==
+
+= 0.1 =
+
+* Initial Version
+* More options will be coming, especially template tags.
